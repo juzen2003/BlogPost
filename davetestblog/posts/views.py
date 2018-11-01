91610ae923099269910e24eca04c21a36fca9ecb
Yu-Jen Chang <juzen2003@gmail.com>
2018-10-31 23:04:18 -0700

    Add QuerySet API to display data info on html

--- davetestblog/posts/views.py
+++ davetestblog/posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'posts/home.html')
+    # use QuerySet API order_by() to query the Post model entries in database
+    posts = Post.objects.order_by('pub_date')
+
+    return render(request, 'posts/home.html', {
+        'posts': posts
+    })
